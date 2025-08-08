6a92f271e2041f9948cca8ec236620d0689a7edc
Ron Melnitcki <79171135+ronmel1888@users.noreply.github.com>
2025-08-08 09:47:37 +0300

    Update mail-controller.js

--- server/controller/mail-controller.js
+++ server/controller/mail-controller.js
@@ -41,7 +41,7 @@ router.post('/send', async (req, res) => {
     // send mail with defined transport object
     let info = transporter.sendMail({
         from: `" ${req.body.name}👻" <ronmel1888@aol.com>`, // sender address
-        to: "ronmeleny@gmail.com", // list of receivers
+        to: "test@test.com, // list of receivers
         subject: `"${req.body.name} sent messege from your website ✔"`, // Subject line
         text: "Hello world?", // plain text body
         html: messege, // html body
